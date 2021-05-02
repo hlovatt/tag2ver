a637a4d525d0ae2f9e0577105c703ee553f8bdb4
howard <howard.lovatt@gmail.com>
2021-05-02 19:44:10 +1000

    "Will change documentation to use python's -m option to run tag2ver."

--- build/lib/tag2ver.py
+++ build/lib/tag2ver.py
@@ -7,7 +7,7 @@ __author__ = "Howard C Lovatt."
 __copyright__ = "Howard C Lovatt, 2020 onwards."
 __license__ = "MIT https://opensource.org/licenses/MIT."
 __repository__ = "https://github.com/hlovatt/tag2ver"
-__version__ = "1.1.16"  # Version set by https://github.com/hlovatt/tag2ver
+__version__ = "1.1.17"  # Version set by https://github.com/hlovatt/tag2ver
 
 __all__ = ['main']
 
