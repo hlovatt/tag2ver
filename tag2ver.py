8fe965a855d229942111da5fec0d24c1a6498f1f
hlovatt <howard.lovatt@gmail.com>
2025-02-21 17:21:23 +1100

    "Format README better on PyPI."

--- tag2ver.py
+++ tag2ver.py
@@ -7,7 +7,7 @@ __author__ = "Howard C Lovatt."
 __copyright__ = "Howard C Lovatt, 2020 onwards."
 __license__ = "MIT https://opensource.org/licenses/MIT."
 __repository__ = "https://github.com/hlovatt/tag2ver"
-__version__ = "1.3.2"  # Version set by https://github.com/hlovatt/tag2ver
+__version__ = "1.3.3"  # Version set by https://github.com/hlovatt/tag2ver
 
 __all__ = ["main"]
 
